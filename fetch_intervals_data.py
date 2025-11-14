3411497bdcfd9263642f64887300542f057fac6d
Rockybalboa1 <50090687+Rockybalboa1@users.noreply.github.com>
2025-11-14 15:09:24 -0500

    Update fetch_intervals_data.py

--- fetch_intervals_data.py
+++ fetch_intervals_data.py
@@ -1,7 +1,7 @@
 import requests
 
-athlete_id = "358634"
-api_key = "734t12g78z7vam8upqt713rf9"
+athlete_id = "<358634>"
+api_key = "<734t12g78z7vam8upqt713rf9>"
 
 url = f"https://intervals.icu/api/v1/athlete/{athlete_id}"
 
